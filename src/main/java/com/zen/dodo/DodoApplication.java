d45ef23999e7494f92ca88569c5be232bb9bc462
ozencelik <ozenc.celik@ceng.deu.edu.tr>
2019-08-07 15:10:15 +0300

    Spring-Backend project initialized.

---
+++ src/main/java/com/zen/dodo/DodoApplication.java
@@ -0,0 +1,13 @@
+package com.zen.dodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoApplication.class, args);
+	}
+
+}
